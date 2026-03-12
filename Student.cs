1719e7bf1cc5024c0d6e981bf24aca825da21582
sanikaghule16 <sanikaghule4@gmail.com>
2026-03-12 20:13:45 +0530

    Create Student.cs Model

---
+++ Student.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCStudent.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
